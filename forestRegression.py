e51140893cb1b0f1db2f8a7c7c5f9465b4def76d
Sadfahlsdj's mac <124646333+Sadfahlsdj@users.noreply.github.com>
2023-11-16 17:20:18 -0800

    substituted solvent name for boiling point--can now run a regression with positive r2 values(wow!)
    added functionality to analyze the data without modulus outlier values that are below 1 (it's a 1 liner on line 194, can be commented out if unwanted)

--- forestRegression.py
+++ forestRegression.py
@@ -58,8 +58,10 @@ import inspect
 
 def forest_regression(df, outputName, solventName):
     # xVars is concentration & curing time; yVars is anisotropy, volume fraction, and modulus
+    # use first yVars for data split by solvent, second one for boiling point data
     xVars = df.drop(['anisotropy-average', 'volume-fraction', 'modulus', 'sample'], axis=1)
-    yVars = df.drop(['concentration', 'curing-time', 'sample'], axis=1)
+    # yVars = df.drop(['concentration', 'curing-time', 'sample'], axis=1)
+    yVars = df.drop(['boiling-point', 'concentration', 'curing-time', 'sample'], axis=1)
     # test_size being 0.3 means that 70% of the data goes into train, which is needed
     xTrain, xValid, yTrain, yValid = train_test_split(xVars, yVars, test_size=0.3, random_state=42)
 
@@ -96,26 +98,46 @@ def forest_regression(df, outputName, solventName):
 
     # r^2, multioutput='raw_values' prints each separately which is what i want
     r2 = metrics.r2_score(yValid, yPred, multioutput='raw_values')
+    r2total = metrics.r2_score(yValid, yPred)
     print(f"R squared score for this data set is {r2} for anisotropy, volume fraction, and modulus respectively in {solventName}")
-
+    print(f"Overall R squared score for this data set is {r2total}")
 
 if __name__ == "__main__":
+    # excelData has solvent names, bPointData(boilingPointData) has boiling points
     excelData = pd.read_csv("excelData.csv", encoding='latin-1')
+    bPointData = pd.read_csv("boilingPointData.csv", encoding='latin-1')
     # print(excelData.shape)
 
     # separating NH and SP data
     # ideally i'd separate the two but there just isn't enough data
 
     columnNames = excelData.columns
+    columnNamesBoilingPoint = bPointData.columns
+
     NHList = pd.DataFrame(columns=columnNames)
+    NHListBoilingPoint = pd.DataFrame(columns=columnNamesBoilingPoint)
+
+    # some modulus values are very small, will try removing them and running again
+    smallModulusListBP = pd.DataFrame(columns=columnNamesBoilingPoint)
+
     for index, row in excelData.iterrows():
         if 'NH' in row['anisotropy-average']:
             tempdf = pd.DataFrame([row])
             # print(tempdf.to_string())
             NHList = pd.concat([NHList, tempdf], ignore_index=True)
 
+    for index, row in bPointData.iterrows():
+        if 'NH' in row['anisotropy-average']:
+            tempdf = pd.DataFrame([row])
+            # print(tempdf.to_string())
+            NHListBoilingPoint = pd.concat([NHList, tempdf], ignore_index=True)
+
 
     excelData = excelData[excelData['anisotropy-average'] != "NH"]
+    bPointData = bPointData[bPointData['anisotropy-average'] != "NH"]
+
+
+    # print(bPointData.to_string())
 
     # separating input list by solvent
     columnNamesNew = excelData.columns
@@ -146,6 +168,8 @@ if __name__ == "__main__":
     # probably a better way to do this to be honest
 
     excelData.drop(["solvent", "polymerization-type"], axis=1, inplace=True)
+    bPointData.drop(["polymerization-type"], axis=1, inplace=True)
+
     nPentaneList.drop(["solvent", "polymerization-type"], axis=1, inplace=True)
     cycloPentaneList.drop(["solvent", "polymerization-type"], axis=1, inplace=True)
     nHexaneList.drop(["solvent", "polymerization-type"], axis=1, inplace=True)
@@ -153,26 +177,24 @@ if __name__ == "__main__":
     nHeptaneList.drop(["solvent", "polymerization-type"], axis=1, inplace=True)
 
 
+
     #print(excelData.to_string())
 
     # cast all to float since output columns that had strings are type Object
     excelData = excelData.astype({"anisotropy-average": float, "modulus": float, "volume-fraction": float})
+    bPointData = bPointData.astype({'anisotropy-average': float, 'modulus': float, 'volume-fraction': float})
     nPentaneList = nPentaneList.astype({"anisotropy-average": float, "modulus": float, "volume-fraction": float})
     cycloPentaneList = cycloPentaneList.astype({"anisotropy-average": float, "modulus": float, "volume-fraction": float})
     nHexaneList = nHexaneList.astype({"anisotropy-average": float, "modulus": float, "volume-fraction": float})
     cycloHexaneList = cycloHexaneList.astype({"anisotropy-average": float, "modulus": float, "volume-fraction": float})
     nHeptaneList = nHeptaneList.astype({"anisotropy-average": float, "modulus": float, "volume-fraction": float})
 
-
-
-    # UNNECCESSARY NOW
-    # excelData.to_csv("chemDataParsed", encoding='utf-8', index=False)
-    # exports parsed csv to a new csv, which is then read from
-    # encoding is (probably) needed to make it work, index means that row index isn't included (good)
-
-    # parsedData = pd.read_csv("nPentane", encoding='latin-1')
-    # forestRegress(parsedData, "anisotropy-average")
+    # some modulus values are much smaller than the others
+    # this modifies the data to get rid of them for testing
+    bPointData = bPointData[(bPointData['modulus']) >= 1]
+    # print(bPointData.to_string())
 
     # second argument is the output to generate a predicted vs actual graph for
     # third argument is solvent name, doesn't need to be exact it's only used for titling
-    forest_regression(nPentaneList, 'anisotropy-average', 'n-pentane')
+    # if using bPointData, it uses all solvents
+    forest_regression(bPointData, 'modulus', 'all solvents overall')
